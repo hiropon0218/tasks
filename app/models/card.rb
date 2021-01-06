9f6e8a41702031d6b1dfabc155fb99e4ba8a6d84
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2021-01-07 00:05:02 +0900

    cardモデルのtitle文字数制限の変更

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,6 +1,6 @@
 class Card < ApplicationRecord
   belongs_to :list
 
-  validates :title, length: {in: 1..255}
+  validates :title, length: {in: 1..30}
   validates :memo,  length: { maximum: 1000}
 end
