c2b645e68493cf97f0f4b46157bcd00f2f017f73
hiropon0218 <74945462+hiropon0218@users.noreply.github.com>
2021-01-09 01:12:17 +0900

    user新規登録時にボードを自動生成する記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :lists, dependent: :destroy
+  # コールバックでユーザー新規登録後に「読了」「読みかけ」カードを生成
+  after_create :create_list
 
   validates :name,   presence: true, length: { maximum: 20 }
+
+  private
+  def create_list
+    lists.create(title: "読了")
+    lists.create(title: "読書中")
+    lists.create(title: "積み本")
+  end
+
 end
